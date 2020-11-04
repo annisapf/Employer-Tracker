aa96710988bad87bad2dbb995392e49696957cf8
Annisa Purbandari <a.purbandari@gmail.com>
2020-11-04 22:14:51 +1100

    add viewEmployees & viewDeparments function

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "mrsbudiman",
+    password: "",
     database: "employer_tracker_db"
 
 });
@@ -40,7 +40,7 @@ connection.connect(function (err) {
 });
 
 
-function start() {
+const start = () => {
     inquirer
         .prompt([
             {
@@ -52,10 +52,104 @@ function start() {
                     "View All Employees by Department",
                     "View All Employees by Role",
                     "Add an Employee",
-                    "Remove an Employee",
-                    "Update Employee Role",
-                    "View All Roles",
+                    "Add a Department",
+                    "Add a Role",
+                    "Update Employee Roles",
                 ]
             }
         ])
+        .then((answer) => {
+            switch (answer.start) {
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+                case "View All Employees by Department":
+                    viewDeparments();
+                    break;
+                case "View All Employees by Role":
+                    viewRoles();
+                    break;
+                case "Add an Employee":
+                    addEmployee();
+                    break;
+                case "Add a Department":
+                    addDepartment();
+                    break;
+                case "Add a Role":
+                    addRole();
+                    break;
+                case "Update Employee Roles":
+                    updateRole();
+                    break;
+
+                default:
+
+                    connection.end();
+            }
+        });
 }
+
+const viewEmployees = () => {
+    const query = "SELECT first_name, last_name FROM employee";
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+};
+
+const viewDeparments = () => {
+    const query = "SELECT * FROM department;"
+    connection.query(
+        query, (err, res) => {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        name: "choice",
+                        type: "list",
+                        message: "Which Department would you like to view?",
+                        choices: () => {
+                            var choiceArray = [];
+                            for (const item of res) {
+                                choiceArray.push(item.name)
+                            }
+                            return choiceArray;
+                        }
+                    }])
+                .then(data => {
+                    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title , department.name, role.salary FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id WHERE ?;"
+                    connection.query(
+                        query, [{
+                            "department.name": data.choice
+                        }], (err, res) => {
+                            if (err) throw err;
+                            console.table(res)
+                            start();
+                        }
+                    )
+                })
+        }
+    )
+};
+
+const viewRoles = () => {
+
+};
+
+const addEmployee = () => {
+
+};
+
+const addDepartment = () => {
+
+};
+
+const addRole = () => {
+
+};
+
+const updateRole = () => {
+
+};
